d1efc229843ca172ff908455166ea0dd0e9467c4
itshahryar <shahryaramajdmos@gmail.com>
2025-12-13 23:12:55 +0500

    ok

--- app/contact/page.js
+++ app/contact/page.js
@@ -3,8 +3,8 @@
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { ArrowUpRight } from "lucide-react";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 
 export default function Contact() {
   const [formData, setFormData] = useState({
